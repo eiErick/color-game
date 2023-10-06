53e4e893570d8d772917f3ac6464e4bba4635bee
Érick <118185219+eiErick@users.noreply.github.com>
2023-10-06 18:41:15 -0300

    init - creating the color guessing game

---
+++ README.md
@@ -0,0 +1,31 @@
+# Calculator JS
+
+This is a color guessing web project made in pure JavaScript.
+
+<img src="img/img-guess-color-game.png" alt="Img guess color game">
+
+## 🔨 Project features
+
+This is a web game with the goal is to try to guess the color by the RGB code, with easy and difficult mode.
+## ✔️ techniques and technologies used 
+
+- `HTML`: HTML serves as a page structuring
+- `CSS`: The CSS gives a more beautiful and modern look.
+- `JavaScript`: JavaScript implements the operations, display, cleaning button and other details.
+  - `querySelector`;
+  - `document`;
+  - `const`;
+  - `let`;
+  - `function`;
+  - `if`;
+  - `event List`;
+  - `forEach`;
+  - `array`;
+  - `DOM`;
+
+## 📁 Access to the project
+
+[See the final project of the course in operation](https://eierick.github.io/color-game).
+
+## 🛠️ Open and run the project
+To open and run the project, just open the `index.html` file in the browser.
---
+++ main.js
@@ -0,0 +1,104 @@
+let btnColor = document.querySelectorAll(".color");
+let header = document.querySelector("#header");
+let scoreboard = document.querySelector("#scoreboard");
+
+const display = document.querySelector("#display");
+const btnReload = document.querySelector("#reload");
+const btnEasy = document.querySelector("#btn-easy");
+const btnHard = document.querySelector("#btn-hard");
+const btnPlayAgain = document.querySelector("#btn-play-again");
+
+let numChooseColor = 6;
+
+showColors();
+
+btnPlayAgain.addEventListener("click", () => {
+    reload();
+    btnPlayAgain.style.display = "none";
+})
+
+btnReload.addEventListener("click", reload)
+
+btnEasy.addEventListener("click", () => {
+    btnEasy.classList.add("selected");
+    btnHard.classList.remove("selected");
+    difficulty("easy");
+})
+
+btnHard.addEventListener("click", () => {
+    btnHard.classList.add("selected");
+    btnEasy.classList.remove("selected");
+    difficulty("hard");
+})
+
+btnColor.forEach(btnColor => {
+    btnColor.addEventListener("click", function() {
+        let btn = this;
+        btn.style.display = "none";
+        verifier(btn);
+        printPoints(-10);
+    })
+});
+
+function generateColors() {
+    const colorRed = parseInt(Math.random() * 256);
+    const colorGreen = parseInt(Math.random() * 256);
+    const colorBlue = parseInt(Math.random() * 256);
+    return `rgb(${colorRed}, ${colorGreen}, ${colorBlue})`;
+}
+
+function chooseColor() {
+    let n = parseInt(Math.random() * numChooseColor);
+    return btnColor[n].style.backgroundColor;
+}
+
+function showColors() {
+    for (var i = 0; i < btnColor.length; i++) {
+        btnColor[i].style.backgroundColor = generateColors();
+    }
+    display.innerHTML = chooseColor().toUpperCase();
+}
+
+function verifier(btnColor) {
+    if (btnColor.style.backgroundColor == display.innerHTML.toLowerCase()) {
+        header.style.backgroundColor = btnColor.style.backgroundColor;
+        btnPlayAgain.style.display = "block";
+        printPoints(30);
+
+        for (var i = 0; i < btnColor.length; i++) {
+            btnColor[i].style.backgroundColor = btn.style.backgroundColor;
+        }
+    }
+}
+
+function reload() {
+    header.style.backgroundColor = "";
+    for (var i = 0; i < btnColor.length; i++) {
+        btnColor[i].style.display = "flex";
+    }
+    showColors();
+}
+
+function printPoints(num) {
+    points = parseInt(scoreboard.innerText);
+    points += num;
+    scoreboard.innerText = points;
+}
+
+function difficulty(difficulty) {
+    if (difficulty == "easy") {
+        btnColor[0].classList.remove("color");
+        btnColor[2].classList.remove("color");
+        btnColor[4].classList.remove("color");
+        btnColor = document.querySelectorAll(".color");
+        numChooseColor = 3;
+        reload();
+    } else {
+        btnColor[0].classList.add("color");
+        btnColor[2].classList.add("color");
+        btnColor[4].classList.add("color");
+        btnColor = document.querySelectorAll(".color");
+        numChooseColor = 6;
+        reload();
+    }
+}
